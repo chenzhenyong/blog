0d6ef08517d84a8cbeae8e0b6ec9cc76e8e2c103
chenzhy <602747631@qq.com>
2018-07-03 16:42:22 +0800

    test

--- src/main/java/com/czy/druiddemo/controller/TestController.java
+++ src/main/java/com/czy/druiddemo/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.czy.druiddemo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +17,10 @@ public class TestController {
     @RequestMapping
     public String hello(){
         return "hello";
+    }
+
+    @GetMapping("/1")
+    public String one(){
+        return "hello 1";
     }
 }
