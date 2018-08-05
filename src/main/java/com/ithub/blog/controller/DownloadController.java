150dad641bbb8fb1002407250982ad6cdff2aeab
chenzhenyong <602747631@qq.com>
2018-08-05 14:57:15 +0800

    change

--- src/main/java/com/ithub/blog/controller/DownloadController.java
+++ src/main/java/com/ithub/blog/controller/DownloadController.java
@@ -1,4 +1,4 @@
-package com.cn21.account.gateway.business.resourcepackage.controller;
+package com.ithub.blog.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
